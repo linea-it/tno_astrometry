16b27062f98af7a473681cf60c98a27e30903bf2
Carlos Adean <carlosadean@gmail.com>
2019-04-26 17:44:35 -0300

    Update praia_header.py

--- praia_header.py
+++ praia_header.py
@@ -20,7 +20,7 @@ def create_input_file(exposures):
 
 def create_params_file(input_file, output_file):
 
-    with open("src/praia_header_params.template.dat") as template:
+    with open(os.path.join(os.getenv("APP_PATH","src/praia_header_params.template.dat")) as template:
 
         data = template.read()
         data = data.replace('{INPUT_FILE}', input_file.ljust(50))
